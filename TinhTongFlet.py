ca0089a25a3018b373bb8b6353ae269f6431e3f9
Do Duc Anh <156392676+Contemporary1408@users.noreply.github.com>
2024-05-11 13:32:44 +0700

    Create TinhTongFlet.py

---
+++ TinhTongFlet.py
@@ -0,0 +1,21 @@
+#Giao diện tính tổng đơn giản bằng Flet
+import flet as ft
+
+def main(page):
+    def btn_click(e):
+        if not txt_name1.value:
+            txt_name1.error_text = "Nhập đúng định dạng số!!"
+            page.update()
+        else:
+            name1 = txt_name1.value
+            name2 = txt_name2.value
+            c = int(name1) + int(name2)
+            page.clean()
+            page.add(ft.Text(f"Tổng cần tính là: {c}"))
+
+    txt_name1 = ft.TextField(label="Số thứ 1")
+    txt_name2 = ft.TextField(label="Số thứ 2")
+
+    page.add(txt_name1)
+    page.add(txt_name2, ft.ElevatedButton("Tính tổng!", on_click=btn_click))
+ft.app(target=main)
