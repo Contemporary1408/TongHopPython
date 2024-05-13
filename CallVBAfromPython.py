62ac8bf80e71b24a90515f2cb97298b434249f66
Do Duc Anh <156392676+Contemporary1408@users.noreply.github.com>
2024-05-13 10:40:03 +0700

    Update CallVBAfromPython.py

--- CallVBAfromPython.py
+++ CallVBAfromPython.py
@@ -1,10 +1,14 @@
 import os, os.path
 import win32com.client
 
-if os.path.exists("D:/python2.xlsm"):
+if os.path.exists("D:/Vendor list-basic.xlsm"):
     xl=win32com.client.Dispatch("Excel.Application")
-    wb = xl.Workbooks.Open(os.path.abspath("D:/python2.xlsm"))
-    xl.Application.Run("python2.xlsm!Module1.prelim")
-    wb.Save() # if you want to save then uncomment this line and change delete the ", ReadOnly=1" part from the open function.
-    xl.Quit() # Comment this out if your excel script closes
+    xl.Visible = True
+    wb = xl.Workbooks.Open(os.path.abspath("D:/Vendor list-basic.xlsm"))
+    #xl.VBE.VBProjects(1).VBComponents.Import('Vendor list-basic.xlsm/UserForm1.frm')
+    #xl.VBE.VBProjects(1).VBComponents.Import('Vendor list-basic.xlsm/Module2.bas')
+    #xl.Run('ShowUserForm')
+    xl.Application.Run("Module2.ShowUserForm")
+    #wb.Save() # if you want to save then uncomment this line and change delete the ", ReadOnly=1" part from the open function.
+    #xl.Quit() # Comment this out if your excel script closes
     del xl
