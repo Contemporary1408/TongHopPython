170ebea15a0cb0d290a3f1f22bd5b517d8a85398
Do Duc Anh <156392676+Contemporary1408@users.noreply.github.com>
2024-05-20 16:48:55 +0700

    Update ParseJSONtoExcel.py

--- ParseJSONtoExcel.py
+++ ParseJSONtoExcel.py
@@ -20,8 +20,6 @@ df.to_excel(r"D:\pysap\weat.xlsx",sheet_name="Forecast" ,engine='openpyxl', inde
 import pandas as pd
 import json
 #source: https://api.weatherapi.com/v1/forecast.json?key=ef1c5c286eb442bc90782533240605&q=Haiphong&days=3&aqi=no&alerts=no
-
-
 # Load your nested JSON data
 with open(r"D:\pysap\forecast.json") as file:
     nested_data = json.load(file)
@@ -32,3 +30,12 @@ df = pd.json_normalize(nested_data1)
 df = df.explode("hour").reset_index(drop=True)
 # Convert the flattened DataFrame to an Excel file
 df.to_excel(r"D:\pysap\weat.xlsx",sheet_name="Forecast" ,engine='openpyxl', index=False)
+
+
+# #######################################
+# For a specific key value:
+with open(r"D:\pysap\forecast.json") as file:
+    nested_data = json.load(file)
+nested_data1 = nested_data['location']
+print(nested_data1['name'])
+print(nested_data1['country'])
