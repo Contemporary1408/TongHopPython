3dcb212f14c13b71ef90227fd7cfe32e30696acc
Do Duc Anh <156392676+Contemporary1408@users.noreply.github.com>
2024-05-22 14:34:24 +0700

    Update JSON_Simplified.py

--- JSON_Simplified.py
+++ JSON_Simplified.py
@@ -9,7 +9,7 @@ print(data['current']['condition']['text']) #print text in condition
 # ################################
 import requests as rq
 import json
-src = rq.get("https://api.weatherapi.com/v1/forecast.json?key=ef1c5c286eb442bc90782533240605&q=Haiphong&days=3&aqi=no&alerts=no",headers=getHeaders())
+src = rq.get("https://api.weatherapi.com/v1/forecast.json?key=ef1c5c286eb442bc90782533240605&q=Haiphong&days=3&aqi=no&alerts=no")
 data = json.load(data.content)
 print(data['forecast']['forecastday'][0]['date']) #print date in forecast
 print(data['current']['condition']['text'])
