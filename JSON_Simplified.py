52a69ba8dc658aafc84b0aec229eb13bc9b38692
Do Duc Anh <156392676+Contemporary1408@users.noreply.github.com>
2024-05-22 14:34:03 +0700

    Update JSON_Simplified.py

--- JSON_Simplified.py
+++ JSON_Simplified.py
@@ -5,3 +5,11 @@ data = json.load(json_file)
 #data1 = json.load(data['forecast']['forecastday'])
 print(data['forecast']['forecastday'][0]['date']) #print 1st date in forecast
 print(data['current']['condition']['text']) #print text in condition
+
+# ################################
+import requests as rq
+import json
+src = rq.get("https://api.weatherapi.com/v1/forecast.json?key=ef1c5c286eb442bc90782533240605&q=Haiphong&days=3&aqi=no&alerts=no",headers=getHeaders())
+data = json.load(data.content)
+print(data['forecast']['forecastday'][0]['date']) #print date in forecast
+print(data['current']['condition']['text'])
