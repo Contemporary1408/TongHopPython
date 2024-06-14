afc5b3c7b97196d1467926ba01de9a33d1016586
Do Duc Anh <156392676+Contemporary1408@users.noreply.github.com>
2024-06-14 09:12:08 +0700

    Create CopyFile_FinalPrice.py

---
+++ CopyFile_FinalPrice.py
@@ -0,0 +1,44 @@
+import tkinter as tk
+from tkinter import filedialog
+import openpyxl as xl
+import ctypes
+import os
+import shutil
+# Make function to retrieve a key's value:
+def get_keyvalue(dictionary, string):
+    for key, value in dictionary.items():
+        if string in key:
+            return value
+    return None
+root = tk.Tk()
+root.withdraw()
+temp_path = filedialog.askopenfilename(title="Chọn đường dẫn template Final-price Excel:")
+wb = xl.load_workbook(temp_path,data_only=True)
+ws = wb.active
+lrow = ws.max_row
+tempdir = os.path.dirname(temp_path)
+# Create folders:
+for i in range(2,lrow+1):
+    foldname = str(ws.cell(row=i,column=4).value.date().strftime("%d-%b"))
+    new_fol = tempdir + "/" + foldname
+    if not os.path.exists(new_fol):
+        os.mkdir(new_fol)
+# Set a dict to mapping file-folder:   
+dict1={}
+for i in range(2,lrow+1):
+    foldname = str(ws.cell(row=i,column=4).value.date().strftime("%d-%b"))
+    filename = str(ws.cell(row=i,column=1).value) + " (final price).xlsx"
+    dict1[filename] = foldname
+# Copy file to respective folder:  
+for path, dirs, files in os.walk(tempdir):
+    for file in files:
+        if file in dict1:
+            src_path = os.path.join(path, file).replace("\\","/")
+            dest_path = os.path.join(tempdir, get_keyvalue(dict1,file),file).replace("\\","/")
+            try:
+                shutil.copyfile(src_path, dest_path)
+
+            # If source and destination are same
+            except shutil.SameFileError:
+                print("Source and destination represents the same file.")
+ctypes.windll.user32.MessageBoxW(0, "Done, 520!", "Thông báo", 0)
