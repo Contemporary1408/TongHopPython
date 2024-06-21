e858a4a752ccdae35fed8cbb18b8feb57428256a
Do Duc Anh <156392676+Contemporary1408@users.noreply.github.com>
2024-06-21 09:04:06 +0700

    Update Awesome.py

--- Snippets/Awesome.py
+++ Snippets/Awesome.py
@@ -17,3 +17,9 @@ if date.today().isoweekday() == 5:
     print("Yes, today is Monday")
 else:
     print("Nope...")
+
+#Formate date https://www.tutorialspoint.com/How-to-get-formatted-date-and-time-in-Python
+from datetime import date
+a = date.today().strftime("%d-%m-%y") #print 21-06-24
+a = date.today().strftime("%d-%b-%y") #print 21-Jun-24
+a = date.today().strftime("%a-%m-%y") #print Fri-06-24
