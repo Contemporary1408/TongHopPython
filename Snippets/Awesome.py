bbf83363a5bd5017d83d436439a292cffe0fa0a4
Do Duc Anh <156392676+Contemporary1408@users.noreply.github.com>
2024-06-22 22:38:03 +0700

    Update Awesome.py

--- Snippets/Awesome.py
+++ Snippets/Awesome.py
@@ -21,6 +21,12 @@ else:
 #Formate date https://www.tutorialspoint.com/How-to-get-formatted-date-and-time-in-Python
 from datetime import date
 t = date(2024,6,17) #assign 17/6/2024 to var t
-a = date.today().strftime("%d-%m-%y") #print 21-06-24
-a = date.today().strftime("%d-%b-%y") #print 21-Jun-24
-a = date.today().strftime("%a-%m-%y") #print Fri-06-24
+a = date.today().strftime("%d-%m-%y") # Output 21-06-24
+a = date.today().strftime("%d-%b-%y") # Output 21-Jun-24
+a = date.today().strftime("%a-%m-%y") # Output Fri-06-24
+
+#Nested list:
+L = ['a', 'b', ['cc', 'dd', ['eee', 'fff']], 'g', 'h']
+print(L[2])         # Output: ['cc', 'dd', ['eee', 'fff']]
+print(L[2][2])      # Output: ['eee', 'fff']
+print(L[2][2][0])   # Output: eee
