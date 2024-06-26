aba2b3eab6c837324a0f7c60ccaaf58cb8c51bc3
Do Duc Anh <156392676+Contemporary1408@users.noreply.github.com>
2024-06-26 11:21:12 +0700

    Update SendEmailwAtt.py

--- SendEmailwAtt.py
+++ SendEmailwAtt.py
@@ -86,6 +86,7 @@ def add_signature_to_email(message, subject, recipient):
     #custom_message = "<p>This is a <b>bold</b> message.</p>" #Bold
     #custom_message = "<p>This is a <u>underlined</u> message.</p>" #Underlined
     #custom_message = "<p>This is a message.<br> </p>" #Add <br> at end of line to break line
+    body += f"<p style='font-size: 10px;'>This automated email is powered by Python.</p>" #Set font size
   
     # Combine the existing signature and your custom message
     mail.HTMLbody = existing_signature + custom_message + mail.HTMLbody[index + 1:]
