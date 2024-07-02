cf4997f3976ef6bb796eebfb075380af4ca043fb
Do Duc Anh <156392676+Contemporary1408@users.noreply.github.com>
2024-07-02 15:29:36 +0700

    Update Simple_SAPLogIn.py

--- Snippets/Simple_SAPLogIn.py
+++ Snippets/Simple_SAPLogIn.py
@@ -1,3 +1,4 @@
+#Basic concept:
 import os
 import win32com.client
 import time
@@ -12,3 +13,39 @@ session.findById("wnd[0]/usr/txtRSYST-BNAME").text = "VHACS12"
 session.findById("wnd[0]/usr/pwdRSYST-BCODE").text = "btmv@140894"
 session.findById("wnd[0]").sendVKey(0)
 session.findById("wnd[0]").maximize()
+
+# ############################################################################################
+#Full script to check whether SAP is opened or not, if opened then open new window:
+import os
+import win32com.client
+import time
+import subprocess
+import psutil
+flag = True
+for process in psutil.process_iter(attrs=['name']):
+    if process.info['name'] == 'saplogon.exe':
+       flag = False
+#Nếu đã đăng nhập SAP:
+if flag == False:      
+    SapGuiAuto = win32com.client.GetObject('SAPGUI')
+    application = SapGuiAuto.GetScriptingEngine
+    connection = application.Children(0)
+    session = connection.Children(0)
+    session.findById("wnd[0]").maximize()
+    session.findById("wnd[0]/tbar[0]/okcd").text = "/oF-65"
+    session.findById("wnd[0]").sendVKey(0)
+#Nếu chưa đăng nhập SAP:    
+else:    
+    path = r"C:\Program Files (x86)\SAP\FrontEnd\SAPgui\saplogon.exe"
+    os.system('start "" "' + path + '"')
+    time.sleep(1)
+    SapGuiAuto = win32com.client.GetObject('SAPGUI')
+    application = SapGuiAuto.GetScriptingEngine
+    connection = application.OpenConnection("BTMV Production Server", True)
+    session = connection.Children(0)
+    session.findById("wnd[0]/usr/txtRSYST-BNAME").text = "VHACS12"
+    session.findById("wnd[0]/usr/pwdRSYST-BCODE").text = "btmv@140894"
+    session.findById("wnd[0]").sendVKey(0)
+    session.findById("wnd[0]").maximize()
+    session.findById("wnd[0]/tbar[0]/okcd").text = "/nZGM07" 
+    session.findById("wnd[0]").sendVKey(0)
