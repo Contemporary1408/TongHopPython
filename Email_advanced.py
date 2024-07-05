bb7086928fb4334e54d116eb66197932880ea046
Do Duc Anh <156392676+Contemporary1408@users.noreply.github.com>
2024-07-05 14:57:58 +0700

    Update Email_advanced.py

--- Email_advanced.py
+++ Email_advanced.py
@@ -3,53 +3,55 @@ import FreeSimpleGUI as sg
 import json
 from datetime import date, timedelta
 import win32com.client as win32
+from suacopy import suacopy
+ico_path =suacopy()
 t = date.today().strftime('%d %b')
 t1 = date.today()+ timedelta(days=1)
 t1 = t1.strftime('%d %b')
 # ##################################################################################
-# Define your list of choices for the combo box
-choices = ['Mẫu 1 - Vanning', 'Mẫu 2 - Hàng xuất', 'Mẫu 3 - Đóng hàng']
-# Create the layout with the combo box and OK button
+# Tạo combo box:
+choices = ['Mẫu 1 - Vanning', 'Mẫu 2 - Tờ khai', 'Mẫu 3 - Kế hoạch']
+# Dành tặng Sứa và mẹ bé Sứa
+# Tạo layout cửa sổ:
 layout = [
     [sg.Text('Chọn mẫu Email:')],
     [sg.Combo(choices, key='combo', size=(50, 1), enable_events=True)],
     [sg.Button('OK')]
 ]
 # Create the window
-window = sg.Window('Tool tạo mail tự động by Do Duc Anh', layout, finalize=True,icon="sua.ico")
+window = sg.Window('Tool tạo mail tự động by Do Duc Anh', layout, finalize=True,icon=ico_path)
 while True:
     event, values = window.read()
 
     if event == sg.WIN_CLOSED:
-        break
+       break
     elif event == 'OK':
         chosen_value = values['combo']
-        break
+        # ##################################################################################
+        with open("mail_loop_adv.json",encoding="utf-8") as file:
+            nested_data = json.load(file)
+        # ##################################################################################
+        outlook = win32.Dispatch('outlook.application')
+        mail = outlook.CreateItem(0)
+        mail.GetInspector()
+        index = mail.HTMLbody.find('>', mail.HTMLbody.find('<body'))
+        existing_signature = mail.HTMLbody[:index + 1]
+        if chosen_value == 'Mẫu 1 - Vanning':
+            mail.To = nested_data['Mau1']['EmailTo']
+            mail.Cc = nested_data['Mau1']['EmailCc']
+            mail.Subject = f"{nested_data['Mau1']['Subject']} {t}"
+            mail.HTMLBody = existing_signature +f"{nested_data['Mau1']['Dear']} "+ mail.HTMLbody[index + 1:]
+            mail.Display()
+        elif chosen_value == 'Mẫu 2 - Tờ khai':
+            mail.To = nested_data['Mau2']['EmailTo']
+            mail.Cc = nested_data['Mau2']['EmailCc']
+            mail.Subject = f"{nested_data['Mau2']['Subject']} {t}"
+            mail.HTMLBody = existing_signature +f"{nested_data['Mau2']['Dear']} {t} "+ mail.HTMLbody[index + 1:]
+            mail.Display()
+        else: 
+            mail.To = nested_data['Mau3']['EmailTo']
+            mail.Cc = nested_data['Mau3']['EmailCc']
+            mail.Subject = f"{nested_data['Mau3']['Subject']} {t1}"
+            mail.HTMLBody = existing_signature +f"{nested_data['Mau3']['Dear']} {t1} "+ mail.HTMLbody[index + 1:]
+            mail.Display()
 window.close()
-# ##################################################################################
-with open("mail_loop_adv.json",encoding='utf-8') as file: #Thêm encoding để nhận ký tự unicode
-    nested_data = json.load(file)
-# ##################################################################################
-outlook = win32.Dispatch('outlook.application')
-mail = outlook.CreateItem(0)
-mail.GetInspector()
-index = mail.HTMLbody.find('>', mail.HTMLbody.find('<body'))
-existing_signature = mail.HTMLbody[:index + 1]
-if chosen_value == 'Mẫu 1 - Vanning':
-    mail.To = nested_data['Mau1']['EmailTo']
-    mail.Cc = nested_data['Mau1']['EmailCc']
-    mail.Subject = f"VANNING // ĐÓNG HÀNG FGWH {t}"
-    mail.HTMLBody = existing_signature +f'Dear Jupiter team,<br><br>Em gửi thông tin cont seal lô Jupiter chạy '+ mail.HTMLbody[index + 1:]
-    mail.Display()
-elif chosen_value == 'Mẫu 2 - Hàng xuất':
-    mail.To = nested_data['Mau2']['EmailTo']
-    mail.Cc = nested_data['Mau2']['EmailCc']
-    mail.Subject = f"HÀNG XUẤT THÀNH PHẨM .... LOADING {t}"
-    mail.HTMLBody = existing_signature +f'Dear Jupiter team,<br><br>Em gửi lô hàng 4705S đóng hàng {t} '+ mail.HTMLbody[index + 1:]
-    mail.Display()
-else: 
-    mail.To = nested_data['Mau3']['EmailTo']
-    mail.Cc = nested_data['Mau3']['EmailCc']
-    mail.Subject = f"KẾ HOẠCH ĐÓNG HÀNG NGÀY {t1}"
-    mail.HTMLBody = existing_signature +f'Dear Jupiter team,<br><br>Em gửi kế hoạch đóng hàng ngày {t1} '+ mail.HTMLbody[index + 1:]
-    mail.Display()
