a58809bef1d0414456d3868fc5d7c72d6c5c5c4e
Do Duc Anh <156392676+Contemporary1408@users.noreply.github.com>
2024-07-17 14:14:16 +0700

    Update ConvertNumToWord.py

--- ConvertNumToWord.py
+++ ConvertNumToWord.py
@@ -1,7 +1,5 @@
-# Install the num2words library (if you haven't already)
-# You can install it using pip:
 # pip install num2words
-
+# NUMBER TO WORD:
 from num2words import num2words
 
 def number_to_vietnamese_words(number):
@@ -11,3 +9,15 @@ def number_to_vietnamese_words(number):
 number = 123456
 words = number_to_vietnamese_words(number)
 print(f"{number} in Vietnamese words: {words}")
+# #################################################################### Vice versa
+# pip install word2number
+# WORD TO NUMBER:
+from word2number import w2n
+
+def number_to_vietnamese_words(number):
+    return w2n.word_to_num(number, lang='vi')
+
+# Example usage
+number = "một trăm hai mươi ba nghìn bốn trăm năm mươi sáu"
+numeric_value = number_to_vietnamese_words(number)
+print(f"{number} in numeric form: {numeric_value}")
