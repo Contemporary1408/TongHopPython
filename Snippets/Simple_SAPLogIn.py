68ee613911e266ac2fa6cd044c4dc8a447543ec0
Do Duc Anh <156392676+Contemporary1408@users.noreply.github.com>
2024-08-06 11:14:26 +0700

    Update Simple_SAPLogIn.py

--- Snippets/Simple_SAPLogIn.py
+++ Snippets/Simple_SAPLogIn.py
@@ -62,3 +62,24 @@ session.findById("wnd[0]/usr/ctxtP_BUKRS").Text = "1000"
 session.findById("wnd[0]/usr/ctxtS_WERKS-LOW").Text = "1000"
 session.findById("wnd[0]/usr/ctxtS_PO_DAT-LOW").Text = "01.01.2020"
 session.findById("wnd[0]/usr/ctxtS_PO_DAT-HIGH").Text = "30.08.2023"
+
+
+# For SAP Business Client ##################################################################
+# Importing the Libraries
+import win32com.client
+import subprocess
+import time
+# This function will Login to SAP from the SAP Logon window
+def saplogin():
+        path = r"C:\Program Files\SAP\NWBC770\NWBC.exe"
+        subprocess.Popen(path)
+        time.sleep(13)
+        SapGuiAuto = win32com.client.GetObject("SAPGUISERVER")
+        application = SapGuiAuto.GetScriptingEngine
+        connection = application.Children(0)
+        session = connection.Children(0)
+        session.findById("wnd[0]/usr/txtRSYST-BNAME").text = "VHACS12"
+        session.findById("wnd[0]/usr/pwdRSYST-BCODE").text = "btmv@140894"
+        session.findById("wnd[0]").sendVKey(0)
+        session.findById("wnd[0]").maximize()
+saplogin()
