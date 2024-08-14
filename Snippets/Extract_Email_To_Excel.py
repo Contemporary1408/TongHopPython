29d01d364a405590fd063ba5c7f81fdbb420369a
Do Duc Anh <156392676+Contemporary1408@users.noreply.github.com>
2024-08-14 09:15:15 +0700

    Update Extract_Email_To_Excel.py

--- Snippets/Extract_Email_To_Excel.py
+++ Snippets/Extract_Email_To_Excel.py
@@ -20,5 +20,7 @@ print("The emails are successfully saved to emails.xlsx file.")
 import re
 setemail = "Nguyen Thi Thu Huong <Huong.Nguyen1@bridgestone.com>; Nguyen Thi Dung 2 <dung.nguyen2@bridgestone.com>; Dong Thi Ninh <dongthininh@bridgestone.com>; Gabriel Goh <gabriel.goh@bridgestone.com>; Le Tien Hung <letienhung@bridgestone.com>; Kelly Qi <kelly.qi@bridgestone.com>; Jia Min Shum <jiamin.shum@bridgestone.com>; Pham Thi Phuong Mai <mai.pham1@bridgestone.com>; Nguyen Thi Ngoc Quynh <nguyenthingocquynh@bridgestone.com>; Nguyen Thi Quynh Trang <trang.nguyen3@bridgestone.com>; Usa Kijsawangsak <usa.kijsawangsak@bridgestone.com>; Do Duc Anh <do.anh@bridgestone.com>"
 matches = re.findall(r'<(.*?)>', setemail)
+listemail=""
 for match in matches:
-    print(match)
+    listemail = listemail + match + ";"
+print(listemail)
